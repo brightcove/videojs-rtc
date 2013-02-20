2d5ebb4bdaa9ded819f4f7eb8a415a3791d82848
Gary Katsevman <git@gkatsev.com>
2013-02-20 14:01:44 -0500

    Move webrtc creation into the plugin to remove one iframe

--- videojsrtc.js
+++ videojsrtc.js
@@ -4,26 +4,56 @@ videojs.plugin('videojsRtc', function(options) {
     iframe = document.createElement('iframe'),
     wrapper = document.createDocumentFragment(),
     wrapperDiv = document.createElement('div'),
-    prop,
-    div;
+    wrapperRtcDiv = document.createElement('div'),
 
-  iframe.src = 'https://videojs-rtc.appspot.com/html/webrtc.html';
+    divJoin = document.createElement('div'),
+    labelJoin = document.createElement('label'),
+    inputText = document.createElement('input'),
+    inputButton = document.createElement('input'),
+
+    divId = document.createElement('div'),
+    labelId = document.createElement('label'),
+    labelKey = document.createElement('label'),
+
+    iframeSrc = 'http://videojs-rtc.appspot.com/',
+    prop,
+    vel = this.el();
+
+  labelJoin.textContent = "Enter room key to join a room:";
+  inputText.type = "text";
+  inputButton.value = "Join Room";
+
+  inputButton.addEventListener('click', function(evt) {
+    var newKey = inputText.value;
+    if (newKey.length > 0) {
+      iframe.src = iframeSrc + "?r=" + newKey;
+    }
+  });
+
+  divJoin.appendChild(labelJoin);
+  divJoin.appendChild(inputText);
+  divJoin.appendChild(inputButton);
+
+  labelId.textContent = "Current room key:";
+
+  divId.appendChild(labelId);
+  divId.appendChild(labelKey);
+
+  window.addEventListener('message', function(evt) {
+    var room_key = JSON.parse(evt.data)['room_key'];
+    labelKey.textContent = room_key;
+  })
+
+  iframe.src = iframeSrc;
   iframe.height = 350;
   iframe.width = 600;
 
-  for (prop in this) {
-    div = this[prop]
-    if (div instanceof HTMLDivElement) {
-      break;
-    } 
-  }
+  wrapperRtcDiv.appendChild(divJoin);
+  wrapperRtcDiv.appendChild(divId);
+  wrapperRtcDiv.appendChild(iframe);
 
-  if (!(div instanceof HTMLDivElement)) {
-    return;
-  }
-
-  div.parentElement.replaceChild(wrapperDiv, div);
-  wrapper.appendChild(div);
-  wrapper.appendChild(iframe);
+  vel.parentElement.replaceChild(wrapperDiv, vel);
+  wrapper.appendChild(vel);
+  wrapper.appendChild(wrapperRtcDiv);
   wrapperDiv.appendChild(wrapper);
 });
